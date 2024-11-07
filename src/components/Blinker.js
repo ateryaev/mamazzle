bff91a13eba9663d45d56be9eb459675f7138a21
Anton Teryaev <unfinder@gmail.com>
2024-11-07 20:26:07 +0200

    Make temporal blinker blink again

--- src/components/Blinker.js
+++ src/components/Blinker.js
@@ -1,18 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export function Blinker({ children, period, times, ...props }) {
   const [counter, setCounter] = useState(0);
+  const visibility = useMemo(() => {
+    if (!times) return counter % 2 === 1;
+    if (counter >= times) return true;
+    return counter % 2 === 1
+  }, [counter, times]);
+
   if (!period) period = 500;
   useEffect(() => {
     const tmo = setTimeout(() => {
-      if (times && counter >= times) return;
+      if (times && counter > times * 10) {
+        setCounter(0);
+        return;
+      }
       setCounter(counter + 1);
     }, period);
     return () => clearTimeout(tmo);
   }, [counter]);
 
   return (
-    <span {...props} style={{ visibility: counter % 2 === 1 ? "hidden" : "visible" }}>
+    <span {...props} style={{ visibility: visibility ? "visible" : "hidden" }}>
       {children}
     </span>
   )
